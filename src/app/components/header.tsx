6f31f59a7611c070c7f2b00f555bd0d163607fbc
christopherleavitt99 <cml95@byui.edu>
2024-05-14 15:24:26 -0600

    created header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,13 @@
+export default function header(){
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Page 1</li>
+                <li>Page 2</li>
+                <li>Page 3</li>
+                <li>Page 4</li>
+            </ul>
+        </nav>
+    )
+}
