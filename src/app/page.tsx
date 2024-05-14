632e260bfa665d84b9392e807c61a54542dadc8e
christopherleavitt99 <cml95@byui.edu>
2024-05-14 15:24:49 -0600

    added button (for practice)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+'use client'
 
 export default function Home() {
+  function handleClick(){
+    console.log('clicked');
+  }
   return (
     <main>
       <h1 className="text-center">Welcome to Plums!</h1>
       <h1>Chris Leavitt</h1>
+      <button onClick={() => handleClick()}>Click Me!</button>
     </main>
   )
 }
