efdbf6201a60dd7ce76aa125e8f0f648eb4057d1
christopherleavitt99 <cml95@byui.edu>
2024-06-04 14:46:50 -0600

    updated page

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,22 +1,15 @@
-import PersonDetail from "@/components/PersonDetail"
+import PersonDetail from "@/components/PersonDetail";
 import Person from "@/data/person.model";
 
 function People() {
-    const people: Person[] =[
-        {name: 'Chris',
-        age: 25
-        },
-        {name: 'Jane',
-        age: 30
-        },
-        {name: 'John',
-        age: 35
-        }
-
-    ];
-    return people.map((person) => (
-        <PersonDetail person={person} />
-    ));
+  const people: Person[] = [
+    { name: "Chris", age: 25 },
+    { name: "Jane", age: 30 },
+    { name: "John", age: 35 },
+  ];
+  return people.map((person) => (
+    <PersonDetail person={person} key={person.name} />
+  ));
 }
 
-export default People
+export default People;
